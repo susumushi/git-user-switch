0e9368d8995f54c8ddaacd40703721c003248f15
susumushi <20012162+susumushi@users.noreply.github.com>
2021-05-15 21:24:34 +0900

    RunEを使った場合、エラーが二重で出力されるので、CheckErrは不要

--- cmd/root.go
+++ cmd/root.go
@@ -37,5 +37,6 @@ to quickly create a Cobra application.`,
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	cobra.CheckErr(rootCmd.Execute())
+	//cobra.CheckErr(rootCmd.Execute())
+	rootCmd.Execute()
 }
