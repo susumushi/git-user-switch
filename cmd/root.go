2e2cd0173e60382312079e78d363551b0212c50a
susumushi <20012162+susumushi@users.noreply.github.com>
2021-05-15 21:33:20 +0900

    エラー出るたびにショートヘルプが出るのはダルいので、無効化

--- cmd/root.go
+++ cmd/root.go
@@ -32,6 +32,7 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+	SilenceUsage: true,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
